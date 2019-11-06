0a85c1a290aeadfe09f2d1f7c6f92827ff30574b
Rising-Moon <767644624@qq.com>
2019-11-06 23:34:39 +0800

    webgl

---
+++ HelloCanvas/HelloCanvas.js
@@ -0,0 +1,12 @@
+﻿function main(){
+    var canvas = document.getElementById("webgl");
+    if(!canvas){
+        return false;
+    }
+    var gl = getWebGLContext(canvas);
+    if(!gl){
+        return null;
+    }
+    gl.clearColor(0.0,0.0,0.0,1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+}
