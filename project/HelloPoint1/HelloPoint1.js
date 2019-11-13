ce71be255d79a9cc1f0c60187be2b814feee6140
RisingMoon <767644624@qq.com>
2019-11-13 10:19:53 +0800

    HelloPoint

---
+++ project/HelloPoint1/HelloPoint1.js
@@ -0,0 +1,32 @@
+﻿var VSHADER_SOUCE =
+    'void main() {\n' +
+    '   gl_Position = vec4(0.0,0.0,0.0,1.0);\n' +
+    '   gl_PointSize = 10.0;\n' +
+    '}\n';
+
+var FSHADER_SOURCE =
+    'void main(){\n' +
+    '   gl_FragColor = vec4(1.0,0.0,0.0,1.0);\n' +
+    '}\n';
+
+function main() {
+    //获取canvas
+    var canvas = document.getElementById('webgl');
+
+    var gl = getWebGLContext(canvas,true);
+    if (!gl) {
+        console.log("浏览器不支持webgl");
+        return;
+    }
+
+    //初始化着色器
+    if (!initShaders(gl, VSHADER_SOUCE, FSHADER_SOURCE)) {
+        console.log("初始化着色器失败");
+        return;
+    }
+
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, 1);
+}
