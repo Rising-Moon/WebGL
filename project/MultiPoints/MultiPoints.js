b5e32516c29dcee6b9782336dc4eb96eb9d2ccb4
RisingMoon <yc_meng@droidhang.com>
2019-12-21 09:38:10 +0800

    no message

--- project/MultiPoints/MultiPoints.js
+++ project/MultiPoints/MultiPoints.js
@@ -1,4 +1,4 @@
-﻿var VSHADER_SOUCE =
+var VSHADER_SOUCE =
     'attribute vec4 a_Position;\n' +
     'attribute float a_PointSize;' +
     'void main() {\n' +
@@ -32,6 +32,7 @@ function main() {
         return;
     }
 
+
     var a_Position = gl.getAttribLocation(gl.program, 'a_Position');
     var a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
     var u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
@@ -39,6 +40,14 @@ function main() {
         console.log("获取a_Position失败");
         return;
     }
+
+    //初始化定点缓冲区
+    var n = initVertexBuffers(gl, a_Position);
+    if (n < 0) {
+        console.log("设置顶点位置失败");
+    }
+
+
     if (a_PointSize < 0) {
         console.log("获取a_PointSize失败");
         return;
@@ -48,30 +57,44 @@ function main() {
         return;
     }
 
-    canvas.onmouseover = function (ev) {
-        click(ev, gl, canvas, a_Position, u_FragColor)
-    };
-
-    canvas.onmousemove = function (ev) {
-        click(ev, gl, canvas, a_Position, u_FragColor)
-    };
-
-    //canvas.onmousedown = function (ev) {
-    //    click(ev, gl, canvas, a_Position)
-    //};
-
+    //设置定点位置
     gl.vertexAttrib1f(a_PointSize, 10.0);
 
     gl.clearColor(0.0, 0.0, 0.0, 1.0);
     gl.clear(gl.COLOR_BUFFER_BIT);
+
+    gl.drawArrays(gl.POINTS, 0, n);
+}
+
+function initVertexBuffers(gl, a_Position) {
+    var vertices = new Float32Array([0.0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+    var n = vertices.length / 2;
+
+    var vertexBuffer = gl.createBuffer();
+    if (!vertexBuffer) {
+        console.log("创建缓冲区失败");
+        return -1;
+    }
+
+    //将缓冲区绑定到目标
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+
+    //向缓冲区对象中写入数据
+    gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
+
+    //将缓冲区对象分配给a_Position变量
+    gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+
+    //链接a_Position变量与分配给它的缓冲区对象
+    gl.enableVertexAttribArray(a_Position);
+
+    return n;
 }
 
 function click(ev, gl, canvas, a_Position, u_FragColor) {
     var x = ev.clientX;
     var y = ev.clientY;
     var rect = ev.target.getBoundingClientRect();
-    //x = ((x - rect.left) - canvas.height / 2) / (canvas.height / 2);
-    //y = (canvas.width / 2 - (y - rect.top)) / (canvas.width / 2);
     x = (x - rect.left) * 2 / canvas.height - 1;
     y = 1 - (y - rect.top) * 2 / canvas.width;
     g_points.push([x, y]);
